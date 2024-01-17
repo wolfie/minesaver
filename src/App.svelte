36e57160788e4f81f3a3bf32ca3968a192e79c0a
Henrik Paul <henrik.paul@gmail.com>
2024-01-18 00:19:32 +0200

    simple solving algorithm

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   const WIDTH = 25;
   const HEIGHT = 25;
-  const MINES = WIDTH * HEIGHT * 0.1;
+  const MINES = WIDTH * HEIGHT * 0.15;
 
   const mines: boolean[] = Array(WIDTH * HEIGHT).fill(false);
   let endGame = false;
@@ -43,30 +43,112 @@
     cellNumbers[row * WIDTH + col] = hits;
     if (hits === 0) {
       revealCell(row - 1, col - 1);
-      revealCell(row, col - 1);
-      revealCell(row + 1, col - 1);
       revealCell(row - 1, col);
-      // revealCell(row, col);
-      revealCell(row + 1, col);
       revealCell(row - 1, col + 1);
+      revealCell(row, col - 1);
+      // revealCell(row, col);
       revealCell(row, col + 1);
+      revealCell(row + 1, col - 1);
+      revealCell(row + 1, col);
       revealCell(row + 1, col + 1);
     }
   };
 
-  const flags: boolean[] = Array(HEIGHT * WIDTH).fill(false);
+  let flags: boolean[] = Array(HEIGHT * WIDTH).fill(false);
+
+  const guess = () => {
+    const surroundings = (row: number, col: number) =>
+      (
+        [
+          [row - 1, col - 1],
+          [row - 1, col],
+          [row - 1, col + 1],
+          [row, col - 1],
+          [row, col + 1],
+          [row + 1, col - 1],
+          [row + 1, col],
+          [row + 1, col + 1],
+        ] as const
+      ).filter(
+        ([row, col]) => row >= 0 && col >= 0 && row < HEIGHT && col < WIDTH
+      );
+
+    const data = cellNumbers
+      .map((n, i) => ({ n, i }))
+      .filter(({ n }) => n >= 1)
+      .map((e) => ({
+        ...e,
+        surroundings: surroundings(Math.floor(e.i / WIDTH), e.i % WIDTH)
+          .map(([row, col]) => {
+            const i = row * WIDTH + col;
+            return {
+              row,
+              col,
+              i,
+              opened: boardReveals[row][col],
+              flagged: flags[i],
+              number: cellNumbers[i],
+            };
+          })
+          .filter(({ opened }) => !opened),
+      }));
+
+    const surrounded = data.filter((e) => e.n === e.surroundings.length);
+    const flaggableCandidates = surrounded.flatMap((e) =>
+      e.surroundings.filter((ee) => !ee.flagged)
+    );
+    const flaggableUniq = Object.values(
+      Object.fromEntries(flaggableCandidates.map((e) => [e.i, e]))
+    );
+
+    if (flaggableUniq.length > 0) {
+      const cell =
+        flaggableUniq[Math.floor(Math.random() * flaggableUniq.length)];
+      flags[cell.i] = true;
+      return;
+    }
+
+    const openableCells = data
+      .filter(
+        (e) =>
+          e.n ===
+          e.surroundings.reduce((sum, ee) => (ee.flagged ? sum + 1 : sum), 0)
+      )
+      .flatMap((e) => e.surroundings.filter((ee) => !ee.flagged && !ee.opened));
+    const openableUniq = Object.values(
+      Object.fromEntries(openableCells.map((e) => [e.i, e]))
+    );
+    if (openableUniq.length) {
+      const cell =
+        openableUniq[Math.floor(Math.random() * openableUniq.length)];
+      revealCell(cell.row, cell.col);
+      return;
+    }
+
+    const unopeneds = cellNumbers
+      .map((n, i) => [n, i] as const)
+      .filter(([n]) => n === -1)
+      .map(([_, i]) => i);
+    const randomUnopened =
+      unopeneds[Math.floor(Math.random() * unopeneds.length)];
+    const row = Math.floor(randomUnopened / WIDTH);
+    const col = randomUnopened % WIDTH;
+    revealCell(row, col);
+  };
 </script>
 
 <main class:endGame>
+  <button on:click={() => guess()}>Next</button>
   {#each boardReveals as boardRow, row (row)}
     <div class="row">
       {#each boardRow as cellIsRevealed, col (col)}
         {#if cellIsRevealed || endGame}
           {#if mines[row * WIDTH + col]}
-            <div class="square revealed">*</div>
+            <div class="square revealed mine">*</div>
           {:else}
+            {@const num = cellNumbers[row * WIDTH + col]}
             <div class="square revealed">
-              {cellNumbers[row * WIDTH + col] || ""}
+              {num >= 1 ? num : ""}
             </div>
           {/if}
         {:else}
@@ -118,5 +200,8 @@
     background-color: rgba(0, 0, 0, 0.125);
     border-color: rgba(0, 0, 0, 0.1);
     cursor: initial;
+  }
+  .square.revealed.mine {
+    background-color: red;
   }
 </style>
